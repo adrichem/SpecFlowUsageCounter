9d35763699b2e4252930c2a974996980e08ecaa5
adrichem <B7QuUOj0KALY@outlook.com>
2022-04-21 09:49:38 +0200

    Ability to include and exclude files in analysis of unused step definitions. (#2)

--- BindingAnalyzerApp/Program.cs
+++ BindingAnalyzerApp/Program.cs
@@ -1,20 +1,55 @@
 ﻿namespace BindingAnalyzer
 {
     using Adrichem.SpecFlowUsageCounter;
+    using Fclp;
+    using Microsoft.Extensions.FileSystemGlobbing;
     using Newtonsoft.Json;
     using System;
+    using System.Collections.Generic;
     using System.IO;
     using System.Linq;
-    using System.Text.RegularExpressions;
+
+    class Arguments
+    {
+        public string Root {get; set; }
+        public List<string> IncludeFeatures { get; set; } = new List<string>();
+        public List<string> IncludeCs { get; set; } = new List<string>();
+        public List<string> ExcludeFeatures { get; set; } = new List<string>();
+        public List<string> ExcludeCs { get; set; } = new List<string>();
+    }
+
 
     class Program
     {
+        static IEnumerable<string> DoGlob(string Root, IList<string> Include, IList<string> Exclude)
+        {
+            Matcher Globber = new();
+            Globber.AddIncludePatterns(Include);
+            Globber.AddExcludePatterns(Exclude);
+            return Globber.GetResultsInFullPath(Root);
+        }
+
         protected Program() { }
         static void Main(string[] args)
         {
-            var root = args.First();
-            var codeFiles = Directory.GetFiles(root, "*.cs", SearchOption.AllDirectories).Where(file => !Regex.IsMatch(file,@".+\.feature.cs$", RegexOptions.IgnoreCase));
-            var FeatureFiles =  Directory.GetFiles(root, "*.feature",SearchOption.AllDirectories );
+            var Parser = new FluentCommandLineParser<Arguments>();
+            Parser.Setup(a => a.Root).As("root").SetDefault(Directory.GetCurrentDirectory()).WithDescription("The path to search for .feature and .cs files.");
+            Parser.Setup(a => a.IncludeFeatures).As("features").SetDefault(new List<string> {"**/*.feature" }).WithDescription("Glob patterns for feature files to include.");
+            Parser.Setup(a => a.IncludeCs).As("code").SetDefault(new List<string> { "**/*.cs" }).WithDescription("Glob patterns for cs files to include.");
+            Parser.Setup(a => a.ExcludeFeatures).As("exclude-features").WithDescription("Glob patterns for feature files to exclude.");
+            Parser.Setup(a => a.ExcludeCs).As("exclude-code").SetDefault(new List<string> { "**/*.feature.cs" }).WithDescription("Glob patterns for cs files to exclude.");
+            Parser.SetupHelp("?", "help").Callback(text => Console.WriteLine(text));
+
+            var ParsingResult = Parser.Parse(args);
+            if (ParsingResult.HasErrors)
+            {
+                Console.Error.WriteLine(ParsingResult.ErrorText);
+                return;
+            }
+            if (ParsingResult.HelpCalled) return;
+
+            var codeFiles = DoGlob(Parser.Object.Root, Parser.Object.IncludeCs, Parser.Object.ExcludeCs);
+            var FeatureFiles = DoGlob(Parser.Object.Root, Parser.Object.IncludeFeatures, Parser.Object.ExcludeFeatures);
             var UnUsedStepDefs = new StepDefinitionUsageCounter()
                 .Analyze(codeFiles, FeatureFiles)
                 .BindingsUsage
@@ -23,7 +58,7 @@
                 .OrderBy(x => x.File)
                 .ThenBy(x=>x.Line)
             ;
-            Console.Out.WriteLine(JsonConvert.SerializeObject(UnUsedStepDefs, Formatting.Indented) );
+            Console.Out.WriteLine(JsonConvert.SerializeObject(new { unused = UnUsedStepDefs, features = FeatureFiles, code = codeFiles }, Formatting.Indented) );
         }
     }
 }
